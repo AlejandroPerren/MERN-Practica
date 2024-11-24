714be47478ce22c0e289ea9b13a08ac97c41bb32
Alejandro <aletomasperren@gmail.com>
2024-11-24 04:08:13 -0300

    swagger function

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -4,10 +4,18 @@ import express,{Express, Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+//ENV
+import dotenv from "dotenv";
+dotenv.config();
+
 //express Config
 const server: Express = express();
 server.use(express.json({limit: '50mb'}));
 server.use(express.urlencoded({extended: true, limit: '50mb'}));
+
+//swagger
+import swaggerUi from "swagger-ui-express";
+
 
 //Security Config
 server.use(helmet());
@@ -15,13 +23,31 @@ server.use(cors());
 
 //Root Router
 import router from "../routes";
+import mongoose from "mongoose";
 
 //Static Server
 server.use(express.static('public'));
 
+//Mongoose Conection
+const DBMONGO = process.env.MONGO_CONNECTION;
+if (!DBMONGO) {
+    throw new Error("MONGO_CONNECTION is not defined in environment variables.");
+}
+mongoose
+    .connect(DBMONGO)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Error connecting to MongoDB:", err));
+
+//swagger config route
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(undefined,{
+    swaggerOptions: {
+        url: "/swagger.json",
+        explorer: true
+    }
+}));
 
 //server in /api
-server.use("/api", router );
+server.use("/api", router);
 
 
 //always api redirect
